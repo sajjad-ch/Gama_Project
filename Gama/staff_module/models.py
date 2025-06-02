bd417517c6ac44ef4ce72524f1d1181e0155d3e3
unknown <sajjadbad2002@gmail.com>
2025-06-02 09:47:08 +0330

    some change in HRNeed Model

--- Gama/staff_module/models.py
+++ Gama/staff_module/models.py
@@ -32,12 +32,11 @@ class Collabrations(models.Model):
 
 
 class HumanRecourceNeed(models.Model):
-    full_name = models.CharField(max_length=128, verbose_name='نام و نام خانوادگی')
+    company_name = models.CharField(max_length=128, verbose_name='نام شرکت')
     email = models.EmailField(verbose_name='ایمیل', null=True, blank=True)
     phone_number = models.CharField(max_length=11, verbose_name='تلفن همراه')
     job_title = models.CharField(max_length=128, verbose_name='عنوان شغلی')
-    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING, verbose_name='دپارتمان')
-    resume_file = models.FileField(upload_to='HR/' ,verbose_name='فایل رزومه', null=True, blank=True)
+
 
     def __str__(self):
         return f'{self.full_name} {self.department}'
