846a410d1e6564adf7bc430689780d7dcc067204
unknown <sajjadbad2002@gmail.com>
2025-07-01 14:00:53 +0330

    adding a new prefix

--- Gama/Gama/urls.py
+++ Gama/Gama/urls.py
@@ -1,32 +1,21 @@
-"""
-URL configuration for Gama project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-urlpatterns = [
-    path("backend/admin/", admin.site.urls),
+backend_patterns = [
+    path("admin/", admin.site.urls),
     re_path(r'^_nested_admin/', include('nested_admin.urls')),
-    path("backend/institute/", include("institute_module.urls")),
-    path("backend/staff/", include("staff_module.urls")),
-    path('backend/api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('backend/api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("institute/", include("institute_module.urls")),
+    path("staff/", include("staff_module.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
+
+urlpatterns = [
+    path("backend/", include(backend_patterns)),
 ]
 
 if settings.DEBUG:
