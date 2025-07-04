7474297d537cb301f3becfcc4d36acb491cfe66f
unknown <sajjadbad2002@gmail.com>
2025-07-04 17:35:55 +0330

    rollback

--- Gama/staff_module/views.py
+++ Gama/staff_module/views.py
@@ -8,10 +8,6 @@ from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-
 # Create your views here.
 
 
@@ -25,14 +21,12 @@ class DepartmentViewSet(viewsets.ModelViewSet):
         return [permissions.AllowAny()]
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class CollabrationsViewSet(viewsets.ModelViewSet):
     queryset = Collabrations.objects.all()
     serializer_class = CollabrationsSerializer
     permission_classes = [permissions.AllowAny]
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class HumanRecourceNeedViewSet(viewsets.ModelViewSet):
     queryset = HumanRecourceNeed.objects.all()
     serializer_class = HumanRecourceNeedSerializer
